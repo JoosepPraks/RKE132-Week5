5f9a8c28d3ff0c6d1bba937ba967db1c59827f25
JoosepPraks <joosep.praks@tktk.ee>
2024-04-23 15:40:15 +0300

    add week5 code

---
+++ Week5/Program.cs
@@ -0,0 +1,11 @@
+﻿// AND Operaator &&
+
+//true && true --> true
+
+// Mõlemad tingimused peavad olema täidetud, et väljund oleks true
+
+// OR (Või)
+
+//true või true --> true
+//true või false --> true
+// false või false --> false
